e9e78a0efff135b49d337ca684cdc8ff868b02ce
Hayden <me@haydenzeller.dev>
2023-08-20 19:45:42 -0400

    api

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -10,7 +10,7 @@ function Home() {
         const addr = await axios.get("https://api.ipify.org/?format=json");
         setIP(addr.data.ip);
 
-        const info = await axios.get("https://api.ipgeolocation.io/ipgeo?apiKey=ccb013f1fb97475e94dcbcdb2192ca7d&ip=" + addr);
+        const info = await axios.get("https://api.ipgeolocation.io/ipgeo?apiKey=ccb013f1fb97475e94dcbcdb2192ca7d&ip=");
         setLocation(info.data.city);
 
     };
