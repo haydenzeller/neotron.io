9dbd211b549f4c071a2e78e968d885ac095c075b
Hayden <haydenzeller@protonmail.com>
2023-11-08 18:15:57 -0500

    build test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.scss'
 import Home from './pages/home/home';
 import NotFound from './pages/404/NotFound'
-import VideoDownloader from './pages/videodownloader/videodownloader'
+import VideoDownloader from './pages/videodownloader/VideoDownloader'
 import { Route, Routes } from "react-router-dom";
 
 function App() {
